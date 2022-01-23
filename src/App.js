fb71765a699ffacfb129d6f37cdca4ade5c40621
Benneth-Bejamas <benneth@bejamas.io>
2022-01-23 17:03:36 +0100

    completing the components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,38 @@ import React from "react";
 import Expandable from "./components/compound/Expandable";
 
 const App = () => {
+  const onExpand = (expanded) => {
+    if (expanded === true) {
+      return true;
+    } else if (expanded === false) {
+      return false;
+    }
+  };
   return (
     <div>
-      <Expandable>
+      <Expandable onExpand={onExpand}>
         <Expandable.Header style={{ color: "red" }}>
           React hooks
         </Expandable.Header>
         <Expandable.Icon />
-        <Expandable.Body>Hooks are awesome</Expandable.Body>
+        <Expandable.Body style={{ color: "red", border: "1px solid teal" }}>
+          {" "}
+          <img
+            src="/api/collection/10370001/4597752283529216/page/5195905143668736/image/4691607934730240"
+            style={{ width: "250px" }}
+            alt="reintroducing react book cover"
+          />
+          <p style={{ opacity: 0.7 }}>
+            This book is so amazing! <br />
+            <a
+              href="https://leanpub.com/reintroducing-react"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Go get it now.
+            </a>
+          </p>
+        </Expandable.Body>
       </Expandable>
     </div>
   );
--- src/components/compound/Expandable.js
+++ src/components/compound/Expandable.js
@@ -9,6 +9,7 @@ import React, {
 import Header from "./Header";
 import Icon from "./Icon";
 import Body from "./Body";
+import "./Expandable.css";
 
 //useEffect is always invoked on mount, we want to be able to toggle this component
 
@@ -16,14 +17,13 @@ export const ExpandableContext = createContext();
 
 const { Provider } = ExpandableContext;
 
-const Expandable = ({ children, onExpand }) => {
+const Expandable = ({ children, onExpand, className = "", ...otherProps }) => {
   const [expanded, setExpanded] = useState(false);
   const toggle = useCallback(
     () => setExpanded((prevExpanded) => !prevExpanded),
     []
   );
   const componentJustMounted = useRef(true);
-  console.log(componentJustMounted);
 
   //this implies that when the component mounts, the ref is true, but when you click, the ref is false and this changes based on the expanded prop.
   // When the component mounts, the ref is true, the onExpanded fn only fires when the ref is false.
@@ -34,7 +34,16 @@ const Expandable = ({ children, onExpand }) => {
     componentJustMounted.current = false;
   }, [expanded]);
   const value = useMemo(() => ({ expanded, toggle }), [expanded, toggle]);
-  return <Provider value={value}>{children}</Provider>;
+
+  const combinedClassNames = ["Expandable", className].join("");
+
+  return (
+    <Provider value={value}>
+      <div className={combinedClassNames} {...otherProps}>
+        {children}
+      </div>
+    </Provider>
+  );
 };
 
 // Remember this is just a personal reference. It's not mandatory
